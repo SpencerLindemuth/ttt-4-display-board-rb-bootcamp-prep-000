4d9f6d78cea4a7736bcc027c5af5068a23f48175
Spencer Lindemuth <lindemut@gmail.com>
2019-03-06 19:56:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,3 +4,5 @@ def display_board(moves)
   puts " #{moves[0]} | #{moves[1]} | #{moves[2]} "
   puts "-----------"
   puts " #{moves[3]} | #{moves[4]} | #{moves[5]} "
+  puts "-----------"
+  puts " #{moves[6]} | #{moves[7]} | #{moves[8]} "
